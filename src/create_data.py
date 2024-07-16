40994035f17ffd509d336d773d36a27372938155
Ella King <eking@rustyamd1.cm.cluster>
2024-07-16 11:40:30 -0400

    add option for continuing data collection

--- src/create_data.py
+++ src/create_data.py
@@ -18,10 +18,10 @@ parser = argparse.ArgumentParser(description='Create data for placenta network a
 parser.add_argument('-f', '--filename', type=str, default="expgrowth")
 parser.add_argument('-el', '--ellipse_ratio', type=float, default=1.0, help='ratio of ellipse axis lengths')
 parser.add_argument('-N', '--N_nodes', type=int, default=10000)
-parser.add_argument('-len', '--edge_len', type=float, default=0.08) #Changing this value causes errors. Unsure why - maybe "overly long lengths" comment in network initialization
+parser.add_argument('-len', '--edge_len', type=float, default=0.08) 
 #parser.add_argument('-Ns', '--N_sinks', type=int, default=90)
 parser.add_argument('-g', '--gamma', type=float, default=0.5)
-parser.add_argument('-ip', '--insertion_point', type=str, default='center', help='center or left')
+parser.add_argument('-ip', '--insertion_point', type=str, default='center', help='center or left or partly_left')
 parser.add_argument('-k', '--N_kappas',type=int, default=10)
 parser.add_argument('-p', '--N_rhos',type=int, default=10)
 parser.add_argument('-nr', '--N_replicates', type=int, default=10)
@@ -49,16 +49,19 @@ if ellipse_ratio==1.0:
 
 # Create a directory to store the data for these input parameters
 run_dir = '../data/' + filename + f'_el{ellipse_ratio}_ip{insertion_point}'
-os.mkdir(run_dir)
+continuing = os.path.exists(run_dir) #is this a continuation of a previous run?
 
+# If this is a new run
+if not continuing:
+    os.mkdir(run_dir)
 
-# Store input parameters in a file in the directory we create for this dataset
-params_str = ""
-for k, v in args.items():
-    params_str += f"{k}: {v}\n"
+    # Store input parameters in a file in the directory we create for this dataset
+    params_str = ""
+    for k, v in args.items():
+        params_str += f"{k}: {v}\n"
 
-with open(run_dir + "/params.txt", "w+") as f:
-    f.write(params_str)
+    with open(run_dir + "/params.txt", "w+") as f:
+        f.write(params_str)
 
 
 # Create a network with the specified number of nodes (Note: we will cut circular/elliptical subsections from this network)
@@ -81,24 +84,31 @@ source_ind = inds[insertion_point]
 kappas = np.logspace(-3, 0, N_kappas)
 rhos = np.logspace(0, 2, N_rhos)
 
+if not continuing:
+    os.mkdir(run_dir + '/Ks/')
+    os.mkdir(run_dir + '/sink_nodes/')
 
-os.mkdir(run_dir + '/Ks/')
-os.mkdir(run_dir + '/sink_nodes/')
 for k in kappas:
     for p in tqdm(rhos):
         for r in range(N_replicates):
-            K0 = -np.log10(np.random.rand(netw.N_e))
-            num_sinks = np.random.randint(N_sinks-5, N_sinks+5) #vary number of sinks within the usual biological range (30-40) (Note: get_sinks returns fewer than the specified #)
 
-            sink_nodes = get_sinks(num_sinks, netw)
-            print('num sinks: ', len(sink_nodes))
-            currents = lambda K, netw: static_currents(K, netw, source_index=source_ind, sink_nodes=sink_nodes)
-            K, converged = ss_solve(lambda K, t: adaptation_ode(K, t, netw, currents, k, beta, p), K0, Δt=1.0)
-            if converged:
-                print('Converged')
+            K_file = run_dir + f'/Ks/K_kappa{np.round(k, 5)}_rho{np.round(p, 5)}_replicate{r}.txt'
+            sink_file = run_dir + f'/sink_nodes/sink_nodes_kappa{np.round(k, 5)}_rho{np.round(p, 5)}_replicate{r}.txt'
 
-            np.savetxt(run_dir + f'/Ks/K_kappa{np.round(k, 5)}_rho{np.round(p, 5)}_replicate{r}.txt', K)
-            np.savetxt(run_dir + f'/sink_nodes/sink_nodes_kappa{np.round(k, 5)}_rho{np.round(p, 5)}_replicate{r}.txt', sink_nodes)
+            if not os.path.exists(K_file):
+
+                K0 = -np.log10(np.random.rand(netw.N_e))
+                num_sinks = np.random.randint(N_sinks-5, N_sinks+5) #vary number of sinks within the usual biological range (30-40) (Note: get_sinks returns fewer than the specified #)
+
+                sink_nodes = get_sinks(num_sinks, netw)
+                print('num sinks: ', len(sink_nodes))
+                currents = lambda K, netw: static_currents(K, netw, source_index=source_ind, sink_nodes=sink_nodes)
+                K, converged = ss_solve(lambda K, t: adaptation_ode(K, t, netw, currents, k, beta, p), K0, Δt=1.0)
+                if converged:
+                    print('Converged')
+
+                np.savetxt(K_file, K)
+                np.savetxt(sink_file, sink_nodes)
 
 
 
